8e9e4846444dc4367d96e226ed18c79c8e71fa03
oh-bom <spdlqj888888@gmail.com>
2024-03-27 17:54:12 +0900

    dfs/bfs :인구 이동 마무리

--- 03.dfs_bfs/기출/b-7.인구 이동 풀이2.py
+++ 03.dfs_bfs/기출/b-7.인구 이동 풀이2.py
@@ -10,15 +10,13 @@ dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 
 def process(x,y,index):
-    print("i",index)
-    #index가 뭐지?
     united=[] #(x,y)와 연합된 나라담는 리스트
     united.append((x,y))
 
     q=deque() #탐색용
     q.append((x,y))
     
-    union[x][y]=index
+    union[x][y]=True
     sum=graph[x][y]
     cnt=1
 
@@ -29,12 +27,12 @@ def process(x,y,index):
             nx=x+dx[i]
             ny=y+dy[i]
 
-            if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
+            if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == False:
                 if l<=abs(graph[nx][ny]-graph[x][y])<=r:
                     q.append((nx,ny))
 
                     #연합에 추가
-                    union[nx][ny]=index
+                    union[nx][ny]=True
                     sum+=graph[nx][ny]
                     cnt+=1
                     united.append((nx,ny))
@@ -46,12 +44,12 @@ def process(x,y,index):
 total_cnt=0
 
 while True:
-    union=[[-1]*n for _ in range(n)]
+    union=[[False]*n for _ in range(n)]
     index=0
 
     for i in range(n):
         for j in range(n):
-            if union[i][j]==-1:
+            if union[i][j]==False:
                 process(i,j,index)
                 index+=1
 
