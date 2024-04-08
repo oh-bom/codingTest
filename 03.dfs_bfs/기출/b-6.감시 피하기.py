5dd83390ba931fa20cb0397036cdf8bbdc12fd84
oh-bom <spdlqj888888@gmail.com>
2024-04-08 16:49:39 +0900

    감시 피하기 수정

--- 03.dfs_bfs/기출/b-6.감시 피하기.py
+++ 03.dfs_bfs/기출/b-6.감시 피하기.py
@@ -10,19 +10,22 @@ for i in range(n):
         if data[i][j]=="T":teacher.append([i,j])
         elif data[i][j]=="X":empty.append([i,j])
 
-def find_student(t,temp):
-    dx=[-1,1,0,0]
-    dy=[0,0,-1,1]
-
-    for i in range(4):
-        nx=t[0]+dx[i]
-        ny=t[1]+dy[i]
- 
-        if 0<=nx and nx<n and 0<=ny<n:
-            if temp[nx][ny]=="S":return True
-            elif temp[nx][ny]!="O":return False
-            elif temp[nx][ny]=="X": 
-                find_student([nx,ny],temp)
+def find_student(t,temp,index):
+    nx,ny=t[0],t[1]
+    if index==0:
+        nx=t[0]+1
+    elif index==1:
+        nx=t[0]-1
+    elif index==2:
+        ny=t[1]+1
+    elif index==3:
+        ny=t[1]-1
+    
+    if 0<=nx<n and 0<=ny<n:
+        if temp[nx][ny]=="S":return True
+        elif temp[nx][ny]=="O":return False
+        elif temp[nx][ny]=="X": 
+            find_student([nx,ny],temp,index)
 
     return False
 
@@ -38,7 +41,8 @@ def solution(teacher,data):
             temp[x[0]][x[1]]="O"
             
         for t in teacher:
-            if find_student(t,temp):return "NO"
+            for i in range(4):
+                if find_student(t,temp,i):return "NO"
 
     return "YES"
 
